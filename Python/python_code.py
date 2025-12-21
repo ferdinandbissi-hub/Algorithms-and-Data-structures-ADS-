e65edbd1117d4aabf1a2251656f78612f06e5a1f
Ferdinand BISSI <ferdinand.bissi@aims-cameroon.org>
2025-12-21 21:37:09 +0100

    Clean up remove_paren function test code
    Removed test code for remove_paren function.

--- Python/python_code.py
+++ Python/python_code.py
@@ -127,9 +127,6 @@ def remove_paren(s):
             list2.append(i)
     result = "".join(reversed(list2))
     return result
-s = ")e(n(glish)(("
-res = remove_paren(s)
-print(res)
 
 # Day 7: Given a string s, sort it in decreasing order based on the frequency of the characters. 
 #The frequency of a character is the number of times it appears in the string.
